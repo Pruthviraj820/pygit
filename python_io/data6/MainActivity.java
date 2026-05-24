bc364b99ac29739c490f276c97f08dddfbb5934b
Pruthviraj Jadhav <7020976174pruthvi@gmail.com>
2026-05-24 23:23:36 +0530

    Add files via upload

---
+++ python_io/data6/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.poe6;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String[] fruits = {"mango" , "banana", "apple" , "grapes"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+
+        ArrayAdapter<String> Adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                fruits
+        );
+
+        listView.setAdapter(Adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String selectedItem = fruits[position];
+
+                Toast.makeText(MainActivity.this , "You clicked" + selectedItem , Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
